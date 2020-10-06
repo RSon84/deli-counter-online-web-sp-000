6ccdf06b94bb8a945fb458514c661dcef3c8c939
Devin Wilson <craigvin84@yahoo.com>
2020-10-06 23:23:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 
 
 def line_simple(array)
-  current_line = "The simple line is currently:"
+  current_line = "The line is currently:"
   array.each.with_index(1) do |value, indexemus|
     current_line << " #{indexemus}. #{value},"
   end
